0bc92e7d7c62e573dc75512ac519c049d80c9a24
Cesar Ballardini <cesar.ballardini@gmail.com>
2021-05-20 17:31:22 -0300

    en lugar de comentar los tests, los marco como blocked

--- tests/test_dos-casamientos.pl
+++ tests/test_dos-casamientos.pl
@@ -16,9 +16,7 @@ test(mi_mujer_es_suegra_de_su_suegro) :- mi_mujer_es_suegra_de_su_suegro.
 
 test(mi_hijastra_es_mi_madre) :- mi_hijastra_es_mi_madre.
 
-/* no anda 
-test(mi_padre_es_mi_yerno) :- mi_padre_es_mi_yerno.
-*/
+test(mi_padre_es_mi_yerno, blocked(no_anda)) :- mi_padre_es_mi_yerno.
 
 test(el_hijo_de_mi_madrastra_es_mi_hermano) :- el_hijo_de_mi_madrastra_es_mi_hermano.
 
@@ -30,21 +28,15 @@ test(el_hijo_de_mi_mujer_es_hermano_de_mi_madre) :- el_hijo_de_mi_mujer_es_herma
 
 test(el_hijo_de_mi_mujer_es_cuñado_de_mi_padre) :- el_hijo_de_mi_mujer_es_cuñado_de_mi_padre.
 
-/* No implementado todavia
-test(el_hijo_de_mi_mujer_es_tio_de_sus_hijos) :- el_hijo_de_mi_mujer_es_tio_de_sus_hijos.
-*/
+test(el_hijo_de_mi_mujer_es_tio_de_sus_hijos, blocked(no_hay_hijos_no_implementado)) :- el_hijo_de_mi_mujer_es_tio_de_sus_hijos.
 
-/* no anda 
-test(mi_mujer_es_suegra_de_su_hija) :- mi_mujer_es_suegra_de_su_hija.
-*/
+test(mi_mujer_es_suegra_de_su_hija, blocked(no_anda)) :- mi_mujer_es_suegra_de_su_hija.
+
 test(yo_soy_padre_de_mi_madre) :- yo_soy_padre_de_mi_madre.
-
 
 test(mi_padre_es_mi_hijo) :- mi_padre_es_mi_hijo.
 
-/* no anda
-test(la_mujer_de_mi_padre_es_mi_hijo) :- la_mujer_de_mi_padre_es_mi_hijo.
-*/
+test(la_mujer_de_mi_padre_es_mi_hijo, blocked(no_anda)) :- la_mujer_de_mi_padre_es_mi_hijo.
 
 test(yo_soy_mi_propio_abuelo) :- yo_soy_mi_propio_abuelo.
 
